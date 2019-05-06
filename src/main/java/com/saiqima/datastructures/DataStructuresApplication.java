e094d3bf7f9ddc2e02bfde78eccfd8202e8c2cce
saiqi.ma <saiqima@yeah.net>
2019-05-06 19:46:37 +0800

    hava a try

--- src/main/java/com/saiqima/datastructures/DataStructuresApplication.java
+++ src/main/java/com/saiqima/datastructures/DataStructuresApplication.java
@@ -3,6 +3,11 @@ package com.saiqima.datastructures;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Description : 扬帆起航
+ * @Author : saiqi.ma
+ * @Date : 2019/5/6 19:43
+ */
 @SpringBootApplication
 public class DataStructuresApplication {
 
