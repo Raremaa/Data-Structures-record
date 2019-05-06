c740a68b06b2a0d834e20009e10cdd6c0ac9249b
saiqi.ma <saiqima@yeah.net>
2019-05-06 17:58:27 +0800

    'start'

---
+++ src/main/java/com/saiqima/datastructures/DataStructuresApplication.java
@@ -0,0 +1,13 @@
+package com.saiqima.datastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saiqima/datastructures/DataStructuresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saiqima.datastructures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataStructuresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
